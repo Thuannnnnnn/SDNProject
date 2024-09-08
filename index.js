6044fdacc0fa5d5f1a5e43b0d7aaccf7f1b6617b
Thuannnnnnn <133732703+Thuannnnnnn@users.noreply.github.com>
2024-09-09 01:10:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const port = 8080;
 app.use(cors());
 app.use(express.json());
 connectDB();
-
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 app.use('/api/user', userRouter);
 
 app.listen(port, () => {
