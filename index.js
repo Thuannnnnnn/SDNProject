bb755676a9318659bdae54af442fda7afabcae9a
Thuannnnnnn <133732703+Thuannnnnnn@users.noreply.github.com>
2024-09-09 00:34:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import connectDB from './config/connectDB.js';
 import userRouter from './router/userRouter.js';
 const app = express();
-const port = 6868;
+const port = 8080;
 
 app.use(cors());
 app.use(express.json());
