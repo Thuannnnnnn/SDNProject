1802a9c831fbe2f45ad577497794146d8265c065
Thuannnnnnn <tranquocthuan2003@gmail.com>
2024-09-09 14:45:49 +0700

    config Azure

--- config/connectDB.js
+++ config/connectDB.js
@@ -6,8 +6,6 @@ const uri = process.env.URIDB;
 const connectDB = async () => {
   try {
     await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
     });
   } catch (err) {
     console.error('Error connecting to MongoDB Atlas:', err);
