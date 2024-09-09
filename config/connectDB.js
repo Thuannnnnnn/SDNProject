326e608b6efd8e1ee8fdc8a92e393b627bee10a3
Thuannnnnnn <tranquocthuan2003@gmail.com>
2024-09-09 11:39:05 +0700

    a

--- config/connectDB.js
+++ config/connectDB.js
@@ -5,9 +5,14 @@ dotenv.config();
 const uri = process.env.URIDB;
 const connectDB = async () => {
   try {
-    await mongoose.connect(uri);
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected successfully');
   } catch (err) {
     console.error('Error connecting to MongoDB Atlas:', err);
   }
 };
+
 export default connectDB;
