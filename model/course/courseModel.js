ac1d32e3896bbe406a39508be9c5e634ef0d8bde
Thuannnnnnn <tranquocthuan2003@gmail.com>
2024-09-23 14:46:55 +0700

    commit

--- model/course/courseModel.js
+++ model/course/courseModel.js
@@ -1,10 +1,35 @@
 import mongoose from "mongoose";
-import contents from "../content/contentModel.js";
+
+const contentSchema = new mongoose.Schema({
+  contentId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  contentName: {
+    type: String,
+    required: true,
+  },
+  contentType: {
+    type: String,
+    required: true,
+    enum: ['videos', 'Docs', 'Question', 'Exam'], // Xác định loại bảng
+  },
+  contentRef: {
+    type: mongoose.Schema.Types.ObjectId,  // Sử dụng ObjectId để tham chiếu
+    refPath: 'contentType',  // Dựa trên contentType để chọn bảng
+    required: true,
+  },
+  createDate: {
+    type: Date,
+    default: Date.now,
+  },
+}, { _id: false });  // _id: false để không tạo id cho mỗi phần tử của contents
 
 const courseSchema = new mongoose.Schema({
   courseId: {
     type: String,
-    require: true,
+    required: true,
     unique: true,
   },
   courseName: {
@@ -34,15 +59,15 @@ const courseSchema = new mongoose.Schema({
   },
   price: {
     type: String,
-    require: true,
+    required: true,
   },
   category: {
     type: String,
-    require: true,
+    required: true,
   },
-  contents: [contents],
+  contents: [contentSchema],  // Sử dụng schema riêng cho nội dung của từng khóa học
 });
 
-const Course = mongoose.model("Courses", courseSchema);
+const Course = mongoose.model("Course", courseSchema);
 
 export default Course;
