3e09885e005658d81cbed7c68017c35941dc9c2c
Thuannnnnnn <tranquocthuan2003@gmail.com>
2024-09-25 11:41:15 +0700

    fix index, config middleware getToken by header

--- index.js
+++ index.js
@@ -4,25 +4,20 @@ import connectDB from "./config/connectDB.js";
 import authRouter from "./router/authRouter.js";
 import uploadRouter from "./router/uploadRouter.js";
 import cookieParser from "cookie-parser";
-//import authMiddleware from "./middleware/authMiddleware.js";
+import authMiddleware from "./middleware/authMiddleware.js";
 import { quizzRouter } from "./router/quizzRouter.js";
 import { specs, swaggerUi } from "./config/swagger.js";
 import courseRouter from "./router/courseRouter.js";
 import morgan from "morgan";
 import contentRouter from "./router/contentRouter.js";
-import examRouter from "./router/examRouter.js"; // Import examRouter
+import examRouter from "./router/examRouter.js";
 import cartRouter from "./router/cartRouter.js";
 const app = express();
 const port = 8080;
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 app.use(express.json());
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  })
-);
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(morgan("combined"));
@@ -30,16 +25,13 @@ connectDB();
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-// app.get('/', (req, res) => {
-//   res.send("Hello");
-// });
-app.use("/api/course", courseRouter);
-app.use("/api/content",contentRouter);
-app.use("/api/cart", cartRouter);
+app.use("/api/course", authMiddleware, courseRouter);
+app.use("/api/content",authMiddleware, contentRouter);
+app.use("/api/cart",authMiddleware, cartRouter);
 app.use("/api/auth", authRouter);
-app.use("/api/quizz", quizzRouter);
-app.use("/api/upload", uploadRouter);
-app.use("/api/exam", examRouter); // Tích hợp examRouter
+app.use("/api/quizz",authMiddleware, quizzRouter);
+app.use("/api/upload",authMiddleware, uploadRouter);
+app.use("/api/exam",authMiddleware, examRouter);
 app.get("/", (req, res) => {
   res.send("Hello, Swagger!");
 });
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,9 +1,14 @@
 import jwt from 'jsonwebtoken';
 import dotenv from "dotenv";
 dotenv.config();
+
 const authMiddleware = (req, res, next) => {
   try {
-    const token = req.cookies.token;
+    const authHeader = req.headers['authorization'];
+    if (!authHeader) {
+      return res.status(401).json({ message: 'Access denied. No token provided.' });
+    }
+    const token = authHeader.split(' ')[1];
     if (!token) {
       return res.status(401).json({ message: 'Access denied. No token provided.' });
     }
