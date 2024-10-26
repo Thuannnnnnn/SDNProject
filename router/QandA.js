e592b1ffbb10b47cf67dd88d60a6380eff82b363
NguyenMinh993 <nguyenminh090903@gmail.com>
2024-10-26 23:41:07 +0700

    Done Exam and QandA

--- router/QandA.js
+++ router/QandA.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express from "express";
 import {
   createQandA,
   getQandAByUserEmail,
@@ -6,34 +6,37 @@ import {
   updateQandA,
   deleteQandA,
   getQandAByCourseId,
-  replyToQandA,  // Route mới cho reply
-  deleteQandAReply // Route mới để xóa reply
-} from '../controller/QandA/QandAController.js';
+  replyToQandA, // Route mới cho reply
+  deleteQandAReply, // Route mới để xóa reply
+  updateQandAReply,
+} from "../controller/QandA/QandAController.js";
 
 const router = express.Router();
 
 // Create QandA (with courseId in URL)
-router.post('/createQandA', createQandA);
+router.post("/createQandA", createQandA);
 
 // Get QandA by user email for a specific course
-router.get('/getQandAByUserEmail:email', getQandAByUserEmail);
+router.get("/getQandAByUserEmail:email", getQandAByUserEmail);
 
 // Get all QandAs
-router.get('/getAllQandA', getAllQandA);
+router.get("/getAllQandA", getAllQandA);
 
 // Get QandA by courseId
-router.get('/getQandAByCourseId/:courseId', getQandAByCourseId);
+router.get("/getQandAByCourseId/:courseId", getQandAByCourseId);
 
 // Update QandA by ID
-router.put('/update/:id', updateQandA);
+router.put("/update/:id", updateQandA);
 
 // Delete QandA by ID
-router.delete('/delete/:id', deleteQandA);
+router.delete("/delete/:id", deleteQandA);
 
 // Route để thêm reply cho QandA
-router.post('/reply/:QandAId', replyToQandA);
+router.post("/reply/:QandAId", replyToQandA);
+
+router.put("/updateReply/:QandAId/:replyId", updateQandAReply);
 
 // Route để xóa reply của QandA
-router.delete('/deleteReply/:QandAId/:replyId', deleteQandAReply);
+router.delete("/deleteReply/:QandAId/:replyId", deleteQandAReply);
 
 export default router;
